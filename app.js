d8ea31628cd3c476b2b81e3de2e6a3e2d67e0d22
664Parvez <parvezrabbi664@gmail.com>
2024-07-13 14:30:45 +0600

    update2

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ const full_year = new Date().getFullYear()
 const copyright_year = document.getElementById("copyright_year")
 
 copyright_year.innerHTML = full_year
+
+
+const hambar = document.getElementById("hambar_icon")
+
+hambar.addEventListener("click", (e) => {
+
+    const responsive_menu = document.querySelector(".responsive_menu")
+    responsive_menu.style.display = "block"
+
+    e.preventDefault()
+})
+
+
+const cross = document.getElementById("cross")
+
+cross.addEventListener("click", () => {
+    const responsive_menu = document.querySelector(".responsive_menu")
+    responsive_menu.style.display = "none"
+})
