eb63beea8977c3ecbf1000a1fb3cdd0eaab59b69
664Parvez <parvezrabbi664@gmail.com>
2024-07-27 12:44:08 +0600

    update

--- app.js
+++ app.js
@@ -21,3 +21,29 @@ cross.addEventListener("click", () => {
     const responsive_menu = document.querySelector(".responsive_menu")
     responsive_menu.style.display = "none"
 })
+
+
+
+
+let sections = document.querySelectorAll(".active_section")
+let navLinks = document.querySelectorAll(".nav-item")
+
+
+window.addEventListener('scroll', () => {
+        let current = '';
+    
+        sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+            if (scrollY >= sectionTop 
+            - section.clientHeight / 3) {
+                current = section.getAttribute('id');
+            }
+        });
+    
+        navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.href.includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
